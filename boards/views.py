98ae4679e2ce5305efbd79f14028109bd26aa278
Sajal <raisulislam541@gmail.com>
2020-02-13 07:37:02 +0600

    add: views for board topics

--- boards/views.py
+++ boards/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def home(request):
     boards = Board.objects.all()
     return render(request, 'Home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
