561c4d8e1e0b8206b5c3905d3caadc5d79e6f162
Sajal <raisulislam541@gmail.com>
2020-04-17 17:13:01 +0600

    [add][form's attribute]

--- boards/forms.py
+++ boards/forms.py
@@ -3,7 +3,10 @@ from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'whats on your mind?'}),
+                              max_length=4000,
+                              help_text="The max length is 4000"
+                              )
 
     class Meta:
         model = Topic
