98b0c0e6616cb0bb19e90d72d539e5c7d9bd812e
Sajal <raisulislam541@gmail.com>
2020-04-17 18:56:13 +0600

    [fix][form validation]

--- boards/views.py
+++ boards/views.py
@@ -20,16 +20,17 @@ def new_topic(request, pk):
     user = User.objects.first()
     if request.method == "POST":
         form = NewTopicForm(request.POST)
-        topic = form.save(commit=False)
-        topic.board = board
-        topic.starter = user
-        topic.save()
-        post = Post.objects.create(
-            message=form.cleaned_data.get('message'),
-            topic=topic,
-            created_by=user
-        )
-        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
