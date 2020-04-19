c493778a955e3c4c154045194c8288d150372ab4
Sajal <raisulislam541@gmail.com>
2020-04-19 21:35:00 +0600

    [add][test for signup]

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,14 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
 from .views import signup
+from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
     def test_signup_status_code(self):
         url = reverse('signup')
         response = self.client.get(url)
@@ -12,3 +17,10 @@ class SignUpTests(TestCase):
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
