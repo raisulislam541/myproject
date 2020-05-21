3ee3b1109d9be36f4028f149878a690f60f892bf
Sajal <raisulislam541@gmail.com>
2020-05-21 15:54:34 +0600

    [add][password reset subject ]

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -2,8 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.models import User
 from django.core import mail
-from django.core.urlresolvers import reverse
-from django.urls import resolve
+from django.urls import resolve, reverse
 from django.test import TestCase
 
 
